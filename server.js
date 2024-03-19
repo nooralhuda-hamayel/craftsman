3ac382998198dbc9b0836390a20597f7e02026e0
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2024-03-19 03:09:46 +0200

    DB structure

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "craftsman"
+    database: "comm_craft"
 })
 
 app.get('/', (req, res) => {
